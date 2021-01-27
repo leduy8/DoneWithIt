698c89f6d9aec93602bd8313f9b2b21efd8278ad
leduy8 <duy.leduc8@gmail.com>
2021-01-27 23:11:37 +0700

    add Account Screen and Listings Screen

--- App.js
+++ App.js
@@ -3,7 +3,13 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
+
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingsScreen />;
 }
